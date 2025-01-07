cd16e957104accb12c4eacf7ddba530a01b757c7
ThiagoCanatoAzevedo <thi.canato@gmail.com>
2025-01-07 10:48:23 -0300

    finalização aula 4

---
+++ aula_3_4/variaveis.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=1, j=2, k;
+    float minha_variavel;
+    
+    char c = 'a'; //char c = 97 --> são iguais
+
+    //casting
+    int inteiro = 10;
+    float inteiro_casting;
+
+    inteiro_casting = (float)inteiro;
+
+    return 0;
+}
